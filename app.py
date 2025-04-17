a3d6fbe392d7e82f79dd05cdc6d0583f18f446fc
rtaylor579 <ryan.taylor@braves.com>
2025-04-17 16:06:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ conn = psycopg2.connect(
     database=st.secrets["DB_NAME"],
     user=st.secrets["DB_USER"],
     password=st.secrets["DB_PASSWORD"],
-    port=st.secrets["DB_PORT"]
+    port=st.secrets["DB_PORT"],
     sslmode='require'
 )
 cursor = conn.cursor()
